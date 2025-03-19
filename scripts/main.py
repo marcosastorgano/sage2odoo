39d1ae8ba3d7f700d69d4bf5e7aad9cb240a71f2
marcosastorgano <marcos.astorgano@scalian.com>
2025-03-19 16:42:31 +0100

    Refactor parser_facturas y main parametrizado, CI listo

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,14 @@
 
 from scripts import parser_facturas, parser_asientos
 
-def main(input_folder='data', output_folder='output', asientos_file='MovimientosAsientosTest.xml'):
-    # Ejecuta parser de facturas
-    parser_facturas.run_parser(input_folder, output_folder)
+def main(input_folder='data', output_folder='output',
+         facturas_file='MovimientosFacturasTest.xml',
+         asientos_file='MovimientosAsientosTest.xml'):
 
-    # Ejecuta parser de asientos contables
+    # Ejecutar parser de facturas
+    parser_facturas.run_parser(input_folder, output_folder, facturas_file)
+
+    # Ejecutar parser de asientos contables
     parser_asientos.run_asientos_parser(input_folder, output_folder, asientos_file)
 
 if __name__ == '__main__':
