3473ab344d14d11875dd59e91941649d27cc0ac0
marcosastorgano <marcos.astorgano@scalian.com>
2025-03-22 15:21:58 +0100

    mapeos de cuentas contables

--- scripts/parser_asientos.py
+++ scripts/parser_asientos.py
@@ -2,8 +2,9 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 import os
+from scripts.mapper import AccountMapper
 
-def parse_asientos(xml_path):
+def parse_asientos(xml_path, mapper):
     tree = ET.parse(xml_path)
     root = tree.getroot()
     data_node = root.find('{urn:schemas-microsoft-com:rowset}data')
@@ -13,11 +14,13 @@ def parse_asientos(xml_path):
     for row in data_node:
         importe = float(row.attrib.get('ImporteAsiento', 0))
         cargo_abono = row.attrib.get('CargoAbono', '').strip().upper()
+        codigo_sage = row.attrib.get('CodigoCuenta')
 
         asiento = {
             'Fecha Asiento': row.attrib.get('FechaAsiento'),
             'Número Asiento': row.attrib.get('Asiento'),
-            'Cuenta Contable': row.attrib.get('CodigoCuenta'),
+            # Aplicamos el mapeo aquí
+            'Cuenta Contable': mapper.map_account(codigo_sage),
             'Descripción': row.attrib.get('Comentario'),
             'Debe': importe if cargo_abono == 'D' else 0.0,
             'Haber': importe if cargo_abono == 'H' else 0.0,
@@ -30,9 +33,12 @@ def parse_asientos(xml_path):
 
     return df_asientos
 
-
 def run_parser(asientos_path, output_folder, output_file_name='asientos_odoo.csv'):
-    df_asientos = parse_asientos(asientos_path)
+    # Cargamos el mapper de cuentas contables
+    mapping_file = os.path.join('mappings', 'equivalencias_sage_odoo.csv')
+    mapper = AccountMapper(mapping_file)
+
+    df_asientos = parse_asientos(asientos_path, mapper)
 
     output_file = os.path.join(output_folder, output_file_name)
 
